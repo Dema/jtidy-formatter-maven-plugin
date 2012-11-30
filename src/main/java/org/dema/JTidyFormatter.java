653de66da0b96b0a9bb05edf6f77b6155f88a693
Dmitry Olyenyov <dmitry.olyenyov@gmail.com>
2012-11-30 18:25:51 +0600

    initial import

---
+++ src/main/java/org/dema/JTidyFormatter.java
@@ -0,0 +1,45 @@
+package org.dema;
+
+/*
+ * Copyright 2001-2005 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+
+import java.util.Properties;
+import org.w3c.tidy.Tidy;
+
+/**
+ * Goal which touches a timestamp file.
+ *
+ * @goal tidy
+ *
+ * @phase process-sources
+ */
+public class JTidyFormatter
+				extends AbstractMojo {
+
+	private Tidy tidy;
+	/**
+	 * JTidy properties
+	 *
+	 * @parameter
+	 */
+	private Properties jTidyProperties;
+
+	public void execute()
+					throws MojoExecutionException {
+	}
+}
